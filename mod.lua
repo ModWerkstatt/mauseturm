a97a95ffc4dddc56465494c5ee934404467c0a0d
Florian Thiel <flth@flth.de>
2023-01-17 22:17:45 +0100

    Betaversion

---
+++ mod.lua
@@ -0,0 +1,19 @@
+function data()
+return {
+    info = {
+      minorVersion = 0,
+      severityAdd = "NONE",
+      severityRemove = "CRITICAL",
+      name = _("mod_name"),
+      description = _("mod_desc"),
+      authors = {
+            {
+                name = "Bandion",
+                role = "CREATOR",
+            },
+	  },	    
+      tags = {"europe", "asset", },
+      visible = true,
+    },
+}
+end
---
+++ strings.lua
@@ -0,0 +1,29 @@
+function data()
+return {
+	
+		de = {
+		
+			["mod_name"] = "Mäuseturm Bingen",
+			["mod_desc"] = "Der Mod enthält den Mäuseturm aus Bingen als Assets.\nDer Binger Mäuseturm ist ein ehemaliger Wehr- und Wachturm, der auf der Mäuseturminsel im Rhein steht. Der Anfang des 14. Jahrhunderts zwecks Eintreibung von Wegezoll (Maut) erbaute Turm erhielt seinen später volkstümlich abgewandelten Namen im Rahmen einer Legende. Enthalten sind drei verschiedene Versionen.",
+
+            ["Mäuseturm Asset"] = "Mäuseturm Asset",
+            ["Mäuseturm zu Bingen"] = "Mäuseturm zu Bingen.",
+            ["Mäuseturm"] = "Bingener Mäuseturm",
+            ["Mäuseturm als Asset"] = "Mäuseturm als Asset. Der Mäuseturm von Bingen steht auf der Mäuseturminsel im Rhein.",
+			
+		},
+		
+		en = {
+		
+			["mod_name"] = "Mäuseturm Bingen",
+			["mod_desc"] = "The mod contains the \"Mouse Tower\" (Mäuseturm) from Bingen as an asset.\nThe Bingen Mäuseturm is a former defence and watchtower that stands on the Mäuseturminsel in the Rhine. Built at the beginning of the 14th century for the purpose of collecting tolls, the tower was later given its popularly modified name as part of a legend. There are three different versions.",
+
+            ["Mäuseturm Asset"] = "Mäuseturm asset",
+            ["Mäuseturm zu Bingen"] = "Mäuseturm from Bingen.",
+            ["Mäuseturm"] = "Mäuseturm",
+            ["Mäuseturm als Asset"] = "Mäuseturm as asset. The \"Mouse Tower\" (Mäuseturm) of Bingen stands on the Mouse Tower Island in the Rhine.",
+            
+		},
+		
+	}
+end
